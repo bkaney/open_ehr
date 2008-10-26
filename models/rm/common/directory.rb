f7e4632ada5237316dbdd0d3ecde0b7a67ef0976
skoba <skoba@277befb4-d6d1-11dc-91ce-cecf2cf5a870>
2008-10-26 06:54:46 +0000

    refs #63
    git-svn-id: http://openehr.jp/svn/ruby/trunk/lib@111 277befb4-d6d1-11dc-91ce-cecf2cf5a870

---
+++ models/rm/common/directory.rb
@@ -0,0 +1,23 @@
+# This module is based on the UML,
+# http://www.openehr.org/uml/release-1.0.1/Browsable/_9_5_1_76d0249_1140536622627_218703_7149Report.html
+# Ticket refs #63
+module OpenEHR
+  module RM
+    module Common
+      module Directory
+        class Folder < OpenEHR::RM::Archetyped::Locatable
+          attr_accessor :items
+          attr_reader folders
+          def initialize(archetype_node_id, name, links, parent=nil, folders=nil, items=nil, uid = nil, archetype_details=nil, feeder_audit=nil)
+            super(archetype_node_id, name, links, parent, uid, archetype_details, feeder_audit)
+            self.items = items
+          end
+          def folders=(folders)
+            raise ArgumentError, "empty subfolder" if !@folders.nil and @folders.empty?
+            @folders = folders
+          end
+        end        
+      end # of Directory
+    end # of Common
+  end # of RM
+end # of OpenEHR
