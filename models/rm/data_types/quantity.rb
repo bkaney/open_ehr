73ff31fe7ef78784d08e68522508273c168ec254
skoba <skoba@277befb4-d6d1-11dc-91ce-cecf2cf5a870>
2009-05-07 12:46:57 +0000

    #refs #50
    git-svn-id: http://openehr.jp/svn/ruby/trunk/lib@122 277befb4-d6d1-11dc-91ce-cecf2cf5a870

--- models/rm/data_types/quantity.rb
+++ models/rm/data_types/quantity.rb
@@ -7,21 +7,34 @@ module OpenEHR
       module Quantity
         class DV_Ordered < OpenEHR::RM::Data_Types::Basic::Data_Value
           include Comparable
-          attr_accessor :other_refference_ranges, :normal_range
-          attr_accessor :normal_status
-          def initialize(normal_range, other_reference_ranges,
+          attr_reader :other_refference_ranges, :normal_range, :normal_status
+
+          def initialize(normal_range=nil, other_reference_ranges=nil,
                          normal_status = nil)
             other_reference_ranges_validity(other_reference_ranges)
             @normal_range = normal_range
             @other_reference_ranges = other_reference_ranges
             @normal_status = normal_status
           end          
+
           def is_normal?
             !normal_range.nil? or !normal_status.nil
           end
+
           def is_simple?
             normal_status.nil? and other_refference_ranges.nil?
           end
+
+          def <=>(other)
+            raise NotImplementError, 'This method should be implemented'
+          end
+
+          def normal_range=(normal_range)
+          end
+
+          def other_reference_ranges=(other_reference_ranges)
+          end
+
           private
           def other_reference_ranges_validity(other_reference_ranges)
             if !other_reference_ranges.nil? or other_reference_ranges.is_empty?
@@ -49,12 +62,25 @@ module OpenEHR
           attr_reader :accuracy
 
           def add(a_diff)
+            raise NotImplementError, 'add must be implemented'
           end
 
           def diff(other)
+            raise NotImplementError, 'diff must be implemented'
           end
 
           def subtract(a_diff)
+            raise NotImplementError, 'subtract must be implemented'
+          end
+        end
+
+        class DV_Amount < DV_Quantified
+          attr_reader :accuracy, :accuracy_is_percent
+          def infix(dv_amount, op)
+            raise NotImplementError, 'infix must be implemented'
+          end
+          def accuracy=(accuracy)
+            raise ArgumentError, 'accuracy invalid'
           end
         end
 
