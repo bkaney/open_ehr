de3c1cb83bd1895051ae07d7af47c2a00917e057
skoba <skoba@277befb4-d6d1-11dc-91ce-cecf2cf5a870>
2009-05-26 10:18:34 +0000

    fixed #59
    git-svn-id: http://openehr.jp/svn/ruby/trunk/lib@129 277befb4-d6d1-11dc-91ce-cecf2cf5a870

---
+++ models/tests/rm/test_data_structure.rb
@@ -0,0 +1,25 @@
+# This module is implemented from this UML:
+#http://www.openehr.org/svn/specification/TAGS/Release-1.0.2/publishing/architecture/computable/UML/uml_start_view.html
+# Ticket refs #59
+require 'test/unit'
+require 'set'
+require 'rm'
+
+
+class RmDataStructureTest < Test::Unit::TestCase
+  def setup
+    name = OpenEHR::RM::Data_Types::Text::DV_Text.new('blood')
+    uid = OpenEHR::RM::Support::Identification::UID_Based_ID.new('ehr::test-1')
+    links = Set.new([uid])
+    assert_nothing_raised(Exception){
+      @data_structure = OpenEHR::RM::Data_Structures::Data_Structure.new('test',name,links) }
+  end
+
+  def test_init
+    assert_instance_of OpenEHR::RM::Data_Structures::Data_Structure, @data_structure
+  end
+
+  def test_data_structure
+    assert_raise(NotImplementedError){@data_structure.as_hierarchy}
+  end
+end
