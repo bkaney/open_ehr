000fada61a4f69a41c48e43f31a0ee46e9305e99
skoba <skoba@277befb4-d6d1-11dc-91ce-cecf2cf5a870>
2009-05-29 03:46:40 +0000

    refs #51
    git-svn-id: http://openehr.jp/svn/ruby/trunk/lib@130 277befb4-d6d1-11dc-91ce-cecf2cf5a870

--- models/rm/data_types/encapslated.rb
+++ models/rm/data_types/encapslated.rb
@@ -1,20 +1,56 @@
+# OpenEHR::RM::Data_Types::Encapsulated 
+# http://www.openehr.org/svn/specification/TAGS/Release-1.0.2/publishing/architecture/computable/UML/uml_start_view.html
+# refs #51
+require 'rubygems'
+require 'locale/info'
+
 module OpenEHR
   module RM
     module Data_Types
       module Encapsulated
         class DV_Encapsulated  < OpenEHR::RM::Data_Types::Basic::Data_Value
-          attr_accessor :charset, :language
+          attr_reader :language, :charset
+
           def size
             raise NotImplementedError, "size method not implemented"
           end
+
+          def language=(language)
+            unless Language::Info.valid_language?(language.code_string)
+              raise ArgumentError, 'invalid language code'
+            end
+            @language = language
+          end
+
+          def charset=(charset)
+            unless charset_valid?(charset)
+              raise ArgumentError, 'invalid charset'
+            end
+            @charset=charset
+          end
+
           private
           def size_positive
             raise ArgumentError, "size must be positive" if size < 0
           end
+
+          def charset_valid?(charset)
+            open('charset.list') do |file|
+              while line = file.gets
+                if charset == line
+                  file.close
+                  return true
+                end
+              end
+            end
+            return false
+          end
         end
+
         class DV_Multimedia < DV_Encapsulated
           
         end
+
         class DV_Parsable < DV_Encapsulated
           attr_reader :value, :formalism
           def initialize(value, formalism, charset = nil, language = nil)
