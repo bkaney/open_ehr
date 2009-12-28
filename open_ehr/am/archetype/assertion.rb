989db85d94300ef386578722e871bee0c7e48193
skoba <skoba@277befb4-d6d1-11dc-91ce-cecf2cf5a870>
2009-12-28 09:58:23 +0000

    refs #73
    git-svn-id: http://openehr.jp/svn/ruby/trunk/lib@386 277befb4-d6d1-11dc-91ce-cecf2cf5a870

--- open_ehr/am/archetype/assertion.rb
+++ open_ehr/am/archetype/assertion.rb
@@ -66,6 +66,38 @@ module OpenEHR
             @reference_type = reference_type
           end
         end
+
+        class OperatorKind
+          OP_EQ = 2001
+          OP_NE = 2002
+          OP_LE = 2003
+          OP_LT = 2004
+          OP_GE = 2005
+          OP_GT = 2006
+          OP_MATCHES = 2007
+
+          OP_NOT = 2010
+          OP_AND = 2011
+          OP_OR = 2012
+          OP_XOR = 2013
+          OP_IMPLIES = 2014
+          OP_FOR_ALL = 2015
+          OP_EXISTS = 2016
+
+          OP_PLUS = 2020
+          OP_MINUS = 2021
+          OP_MULTIPLY = 2022
+          OP_DIVIDE = 2023
+          OP_EXP = 2024
+
+          def self.valid_operator?(value)
+            if value >= OP_EQ && value <= OP_EXP
+              return true
+            else
+              return false
+            end
+          end
+        end
       end # of Assetion
     end # of Archtype
   end #of AM
