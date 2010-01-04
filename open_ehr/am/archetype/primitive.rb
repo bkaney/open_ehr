ffed409c6708c733d40129a44135c509205c23bd
skoba <skoba@277befb4-d6d1-11dc-91ce-cecf2cf5a870>
2010-01-04 00:57:49 +0000

    refs #71
    git-svn-id: http://openehr.jp/svn/ruby/trunk/lib@391 277befb4-d6d1-11dc-91ce-cecf2cf5a870

--- open_ehr/am/archetype/primitive.rb
+++ open_ehr/am/archetype/primitive.rb
@@ -1,9 +1,28 @@
+
+
 module OpenEHR
   module AM
     module Archetype
       module Primitive
         class CPrimitive
+          attr_reader :default_value
+          attr_accessor :assumed_value
 
+          def initialize(args = { })
+            self.default_value = args[:default_value]
+            self.assumed_value = args[:assumed_value]
+          end
+
+          def default_value=(default_value)
+            if default_value.nil?
+              raise ArgumentError, 'default_value is mandatory'
+            end
+            @default_value = default_value
+          end
+
+          def has_assumed_value?
+            return !assumed_value.nil?
+          end
         end
       end # of Primitive
     end # of Archetype
